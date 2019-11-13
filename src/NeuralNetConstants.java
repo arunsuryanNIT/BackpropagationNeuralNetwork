76e145edc5186fcfbbe1ac4ae6d70055f2913f3f
Arun Suryan <arunsuryan250619@gmail.com>
2019-11-13 16:33:11 +0530

    AND Operation working good now (SIGMOID NEURON)

---
+++ src/NeuralNetConstants.java
@@ -0,0 +1,9 @@
+public class NeuralNetConstants {
+    private NeuralNetConstants() {
+
+    }
+
+    public static final float LEARNING_RATE = 0.3f;
+    public static final float MOMENTUM = 0.3f;
+    public static final int ITERATIONS = 100000;
+}
